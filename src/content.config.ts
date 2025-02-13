a2035f802ef38c69aa23eafd791e217c1d74a4e5
wenquansun <604164227@qq.com>
2025-02-13 17:58:07 +0800

    feat: tag

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,27 @@
 // Import the glob loader
-import { glob } from "astro/loaders";
+import { glob } from 'astro/loaders'
 // Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection } from 'astro:content'
+
+const schema = z.object({
+  title: z.string(),
+  pubDate: z.date(),
+  description: z.string(),
+  author: z.string(),
+  image: z.object({
+    url: z.string(),
+    alt: z.string(),
+  }),
+  tags: z.array(z.string()),
+})
+
 // Define a `loader` and `schema` for each collection
 const blog = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/blog" }),
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    tags: z.array(z.string()),
-  }),
-});
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/blog' }),
+  schema,
+})
+
+export type MarkdownFrontmatterType = z.infer<typeof schema>
+
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { blog }
